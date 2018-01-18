063a35eabf98237c2e9ff3f56288f8669ff8376b
unknown <uid12409@sansi.com>
2018-01-18 18:43:59 +0800

    Learning of day_01 hello python

---
+++ day_01/test.py
@@ -0,0 +1,9 @@
+name=input('please input your name:')
+print('hello,world',name)
+
+print('1024*768=',1024*768)
+print('代泽杨，你好！')
+print('''line1 
+... line2''')
+print('''line1 line1
+line2 line 1''')
